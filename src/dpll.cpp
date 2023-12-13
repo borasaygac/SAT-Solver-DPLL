bb45f982642776fa059c6132fab962458071435a
saygac <saygac@cip.ifi.lmu.de>
2023-12-13 23:36:11 +0100

    couple changes

--- src/dpll.cpp
+++ src/dpll.cpp
@@ -3,30 +3,44 @@
 
 #include "../include/cnf.hpp"
 #include <iostream>
+#include <queue>
 
 bool dpll(std::vector<Clause> &cnf, std::vector<Variable> &variables) {
   // While loop
   int count = 1;
-  *(variables[1].val) = true;
-
-  for (int i = 0, size = variables[count].pos_occ.size(); i < size; i++) {
-    if (cnf[variables[count].pos_occ[i]].satLiteral != 0) {
-      cnf[variables[count].pos_occ[i]].satLiteral = 1;
+  std::queue<int> q;
+  while(count != numOfVars){
+    if(*(variables[count].val) == true || *(variables[count].val) == false){
+      continue;
     }
-  }
+    *(variables[count].val) = true;
+    
+    //positive occurances
+    for (int i = 0, size = variables[count].pos_occ.size(); i < size; i++) {
+      if (cnf[variables[count].pos_occ[i]].satLiteral != 0) {
+        cnf[variables[count].pos_occ[i]].satLiteral = count;
+      }
+    }
 
-  for (int i = 0, size = variables[count].neg_occ.size(); i < size; i++) {
-    // check if clause fullfilled
-    if (cnf[variables[count].neg_occ[i]].satLiteral == 0) {
+    // for every unsatisfied clause C in neg_occ 
+    for (int i = 0, size = variables[count].neg_occ.size(); i < size; i++) {
+      // decrement active
       cnf[variables[count].neg_occ[i]].active--;
-    }
+    
+      // if act == 1 -> find free unique lit. a in C  
+      if (cnf[variables[count].neg_occ[i]].active == 1) {
+        for (int vars: cnf[variables[count].neg_occ[i]].literals){
+          if(vars == 0){
+            q.push(vars);
+          } 
+        }
+      }
 
-    // queue
-    if (cnf[variables[count].neg_occ[i]].active == 1) {
+      // Backtracking and report conflict
+      if (cnf[variables[count].neg_occ[i]].active == 0) {
+      }
     }
-    // Backtracking and report conflict
-    if (cnf[variables[count].neg_occ[i]].active == 0) {
-    }
+    count++;
   }
   std::cout << "P";
   return true;
--- src/main.cpp
+++ src/main.cpp
@@ -59,8 +59,10 @@ void parseDIMACS(const std::string &filename) {
 
 int main(int argc, char *argv[]) {
 
-  std::string filename =
-      "DIMACS/test" + std::to_string(std::stoi(argv[1])) + ".cnf";
+  std::string filename = "C:/Users/boras/Desktop/inputs/test/sat/xor.cnf";
+      //"DIMACS/test" + std::to_string(std::stoi(argv[1])) + ".cnf";
+
+  
 
   parseDIMACS(filename);
   std::cout << "Params: vars " << numOfVars << " and clauses " << numOfClauses
