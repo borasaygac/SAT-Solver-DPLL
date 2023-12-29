fc32eeca36e084074ea89aa4912a3ce3300fd8c0
Lemar <abawi@cip.ifi.lmu.de>
2023-12-29 19:48:30 +0100

    remove bug

--- src/checkAllClauses.cpp
+++ src/checkAllClauses.cpp
@@ -23,16 +23,16 @@ bool checkAllClauses() {
     std::cout << "[";
     for (int i = 1; i < numOfVars; i++) {
         int val;
-        if (variables[i].val == FREE) val = 1000;
-        if (variables[i].val == TRUE) val = i;
-        if (variables[i].val == FALSE) val = -i;
+        if (variables[i].getValue() == FREE) val = 1000;
+        if (variables[i].getValue() == TRUE) val = i;
+        if (variables[i].getValue() == FALSE) val = -i;
 
         std::cout << val << ", ";
     }
     int val;
-    if (variables[numOfVars].val == FREE) val = 1000;
-    if (variables[numOfVars].val == TRUE) val = numOfVars;
-    if (variables[numOfVars].val == FALSE) val = -numOfVars;
+    if (variables[numOfVars].getValue() == FREE) val = 1000;
+    if (variables[numOfVars].getValue() == TRUE) val = numOfVars;
+    if (variables[numOfVars].getValue() == FALSE) val = -numOfVars;
     std::cout << val;
     std::cout << "]";
     return false;
