e9f20fd862980626fe115ea8d59f1652c569e826
Lemar <abawi@cip.ifi.lmu.de>
2024-01-03 13:40:30 +0100

    add parameters to scirpt

--- script.ps1
+++ script.ps1
@@ -1,7 +1,13 @@
 # Iterate from 1 to 10 and call make run with arg=x
-for ($i = 146; $i -le 153; $i++) {
-    & ./main c$i
+param (
+    [int]$start,
+    [int]$end,
+    [char]$type
+)
+
+for ($i = $start; $i -le $end; $i++) {
+    & ./main $type$i
 }
-# .\script.ps1
+# .\script.ps1 -start 1 -end 10 -type 'c'
 # Set-ExecutionPolicy -Scope Process Bypass
 # powershell -ExecutionPolicy Bypass -File ".\script.ps1"
