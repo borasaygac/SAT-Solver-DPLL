0bc3eaa780a6bf78ff5b5b62c506d9b869d011f9
Lemar <abawi@cip.ifi.lmu.de>
2024-01-17 20:59:48 +0100

    int

--- run.ps1
+++ run.ps1
@@ -2,14 +2,25 @@ param (
     [char]$type,
     [int]$start,
     [int]$end,
-    [int]$heur = 0
+    [int]$heur = 42
 )
+if ($heur -eq 42) {
 
-for ($i = $start; $i -le $end; $i++) {
-    & ./main $type$i $heur
-    Start-Sleep -Milliseconds 1000
+    for ($test = $start; $test -le $end; $test++) {
+        for ($h = 0; $h -le 3; $h++) {
+            & ./main $type$test $h
+            Start-Sleep -Milliseconds 1000
+        }
+        Write-Host "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+    }
+}
+else {
+    for ($i = $start; $i -le $end; $i++) {
+        & ./main $type$i $heur
+        Start-Sleep -Milliseconds 1000
+    }
 }
 
-# .\run.ps1 -type 'c' -start 1 -end 10 -heur 1
+# .\run.ps1 -type 'c' -start 1 -end 10 -heur 1 
 
 # Set-ExecutionPolicy -Scope Process Bypass
