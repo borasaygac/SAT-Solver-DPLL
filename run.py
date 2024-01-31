62d557b3575367ceb5aca7a13dcf9302aaee3f22
Lemar <abawi@cip.ifi.lmu.de>
2024-01-31 14:04:02 +0100

    add run

--- run.py
+++ run.py
@@ -3,10 +3,10 @@ import sys
 import time
 
 
-def param(type, start, end, heur=4 timeout_value=600):
+def param(type, start, end, heur=999, timeout_value=600):
     for i in range(start, end + 1):
 
-        if heur == 42:
+        if heur == 999:
             for h in range(4):
                 try:
                     subprocess.run(
@@ -14,7 +14,7 @@ def param(type, start, end, heur=4 timeout_value=600):
                     time.sleep(1)
                 except subprocess.TimeoutExpired:
                     print(
-                        f"Timeout of {timeout_value} seconds reached for subprocess with input {type}{i}.")
+                        f"Timeout of {timeout_value} seconds reached.")
             print("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
 
         else:
@@ -28,13 +28,22 @@ def param(type, start, end, heur=4 timeout_value=600):
 
 
 if __name__ == "__main__":
-    script_name, script_type, start_value, end_value, heur_value = sys.argv
-
-    try:
-        start_value, end_value, heur_value = map(
-            int, (start_value, end_value, heur_value))
-    except ValueError:
-        print("Error: start, end, heur, and timeout must be integers.")
-        sys.exit(1)
+    heur_value = 999
+    if(len(sys.argv) == 5):
+        script_name, script_type, start_value, end_value, heur_value = sys.argv
+        try:
+            start_value, end_value, heur_value = map(
+                int,(start_value, end_value, heur_value))
+        except ValueError:
+            print("Error: start, end, heur, and timeout must be integers.")
+            sys.exit(1)
+    else:
+        script_name, script_type, start_value, end_value = sys.argv
+        try:
+            start_value, end_value = map(
+                int,(start_value, end_value))
+        except ValueError:
+            print("Error: start, end, heur, and timeout must be integers.")
+            sys.exit(1)
 
     param(script_type, start_value, end_value, heur_value)
